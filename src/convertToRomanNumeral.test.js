4255e34b76742c102393adda36aaba50ea301e5b
DH <dh@mbp1.local>
2021-11-10 12:09:30 +0000

    Initial commit project setup adding jest for red green refactor tdd kata
    Added first failing test for input 1 to return "I"

---
+++ src/convertToRomanNumeral.test.js
@@ -0,0 +1,5 @@
+describe('roman numeral convertor' , () => {
+    it('should return "I" when I call the convertor with "1"', () => {
+        expect(romanNumeralConvertor(1)).toBe('I');
+    })
+});
