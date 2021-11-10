062c56e159575af2b4ed414921e91bd9697cdd13
DH <dh@mbp1.local>
2021-11-10 12:53:45 +0000

    test 2 when 2 is passed it should return 'II'

--- src/convertToRomanNumeral.test.js
+++ src/convertToRomanNumeral.test.js
@@ -1,9 +1,16 @@
-function romanNumeralConvertor() {
+function romanNumeralConvertor(num) {
+    if(num === 2) {
+        return "II"
+    }
     return "I"
 }
 
 describe('roman numeral convertor' , () => {
     it('should return "I" when I call the convertor with 1', () => {
         expect(romanNumeralConvertor(1)).toBe('I');
+    })
+
+    it('should return "II" when I call the convertor with 2', () => {
+        expect(romanNumeralConvertor(2)).toBe('II');
     })
 });
