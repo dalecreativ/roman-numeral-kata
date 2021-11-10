e03f4297e679f596b6c3d7e4bd573b81b61c4a21
DH <dh@mbp1.local>
2021-11-10 17:18:05 +0000

    refactor numeral function to class

--- src/convertToRomanNumeral.test.js
+++ src/convertToRomanNumeral.test.js
@@ -1,40 +1,49 @@
-function romanNumeralConvertor(num) {
-    if(typeof num !== "number") {
-        return "Error - input invalid. The input provided is not a Number"
+class RomanNumeralGenerator {
+    constructor(num) {
+        this.num = num;
+        this.output = '';
+        this.lookupIndex = 0;
+        this.romanLookup = [
+            {arabic: 1000, numeral: 'M'},
+            {arabic: 900, numeral: 'CM'},
+            {arabic: 500, numeral: 'D'},
+            {arabic: 400, numeral: 'CD'},
+            {arabic: 100, numeral: 'C'},
+            {arabic: 90, numeral: 'XC'},
+            {arabic: 50, numeral: 'L'},
+            {arabic: 40, numeral: 'XL'},
+            {arabic: 10, numeral: 'X'},
+            {arabic: 9, numeral: 'IX'},
+            {arabic: 5, numeral: 'V'},
+            {arabic: 4, numeral: 'IV'},
+            {arabic: 1, numeral: 'I'}
+        ];
     }
 
-    if(num < 1 || num > 3999) {
-        return "Error - invalid range. The input provided should be between 0 and 3999"
-    }
-
-    let output = '';
-    let lookupIndex = 0;
-    const romanLookup = [
-        {arabic: 1000, numeral: 'M'},
-        {arabic: 900, numeral: 'CM'},
-        {arabic: 500, numeral: 'D'},
-        {arabic: 400, numeral: 'CD'},
-        {arabic: 100, numeral: 'C'},
-        {arabic: 90, numeral: 'XC'},
-        {arabic: 50, numeral: 'L'},
-        {arabic: 40, numeral: 'XL'},
-        {arabic: 10, numeral: 'X'},
-        {arabic: 9, numeral: 'IX'},
-        {arabic: 5, numeral: 'V'},
-        {arabic: 4, numeral: 'IV'},
-        {arabic: 1, numeral: 'I'}
-    ]
-
-    while(num > 0) {
-        const subtract = romanLookup[lookupIndex].arabic;
-        if(subtract <= num) {
-            output += romanLookup[lookupIndex].numeral;
-            num = num - subtract
-        } else{
-            lookupIndex++
+    get romanNumeral() {
+        if(typeof this.num !== "number") {
+            return "Error - input invalid. The input provided is not a Number"
         }
+    
+        if(this.num < 1 || this.num > 3999) {
+            return "Error - invalid range. The input provided should be between 0 and 3999"
+        }
+        return this.convertToNumerals();
     }
-    return output;
+
+    convertToNumerals(){
+        while(this.num > 0) {  
+            const subtract = this.romanLookup[this.lookupIndex].arabic;
+            if(subtract <= this.num) {
+                this.output += this.romanLookup[this.lookupIndex].numeral;
+                this.num = this.num - subtract
+            } else{
+                this.lookupIndex++
+            }
+        }
+        
+        return this.output;
+    }
 
 }
 
@@ -65,11 +74,10 @@ const testCases = [
     [4000, "Error - invalid range. The input provided should be between 0 and 3999"]
 ]
 
-
-describe('roman numeral convertor' , () => {
+describe('roman numeral convertor class implementation' , () => {
 
     it.each(testCases)('should convert %i to %s ', (input, expected) => {
-        const result =  romanNumeralConvertor(input)
+        const result =  new RomanNumeralGenerator(input).romanNumeral
         expect(result).toBe(expected)
     })
     
